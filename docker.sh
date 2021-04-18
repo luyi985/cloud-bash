e109ad0d3a85f1f26b6204b8fa41459ca655ee43
Yi Lu <luyi985@gmail.com>
2021-04-18 18:27:02 +1000

    feat: docker install/uninstall

---
+++ docker.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic test"
+
+sudo apt-get update &&
+sudo apt-get upgrade
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=armhf signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+sudo systemctl enable docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+function uninstallDocker () {
+    sudo apt-get purge docker-ce docker-ce-cli containerd.io \
+    sudo rm -rf /etc/docker &&
+    sudo rm -rf /var/lib/docker &&
+    sudo rm -rf /var/lib/containerd
+}
