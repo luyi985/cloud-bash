fcbecac8bdd1a2565c8eb1e628c262dddf7a2341
Yi Lu <luyi985@gmail.com>
2021-04-18 23:58:07 +1000

    feat: single cluster.1

--- kubernetes.sh
+++ kubernetes.sh
@@ -43,3 +43,45 @@ function uninstall () {
     sudo apt-get autoremove  
     sudo rm -rf ~/.kube
 }
+
+function clusterSingle() {
+    swapoff -a
+
+    # https://www.redpill-linpro.com/techblog/2019/04/04/kubernetes-setup.html
+
+    # The --pod-network-cidr setting is required by Flannel, which I chose to use for pod networking.
+    kubeadm init --pod-network-cidr=10.244.0.0/16
+
+    # There is still a bunch of work to do to make the cluster actually useful. 
+    # You can do most of the rest of this as a non-root user. 
+    # Follow the instructions kubeadm gave you to copy the credential as your regular user.
+
+    mkdir -p $HOME/.kube
+    sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    sudo chown $(id -u):$(id -g) $HOME/.kube/config
+    source <(kubectl completion bash)
+
+    # Install Flannel for pod networking
+    # https://github.com/flannel-io/flannel/blob/master/Documentation/kube-flannel.yml
+    kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+    kubectl get pods --all-namespaces
+
+    # Untaint the master so you can run pods
+    # At this point you can run pods and expose them with services. If that’s all you need, you’re done
+    kubectl taint nodes --all node-role.kubernetes.io/master-
+
+    # Set up nginx-ingress
+    # https://kubernetes.github.io/ingress-nginx/deploy/#bare-metal
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.45.0/deploy/static/provider/baremetal/deploy.yaml
+cat > nginx-host-networking.yaml << EOF
+spec:
+  template:
+    spec:
+      hostNetwork: true
+EOF
+    kubectl -n ingress-nginx patch deployment ingress-nginx-controller --patch="$(<nginx-host-networking.yaml)"
+
+    # dashboard
+    # https://github.com/kubernetes/dashboard
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.2.0/aio/deploy/recommended.yaml
+}
